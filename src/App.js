1cc687763b7d4b9e607e7f80b23110752fbba3d0
Michael Chacaton <chako256@gmail.com>
2017-09-13 16:39:43 +0200

    Adding header component to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import BlogHeaderComponent from './components/BlogHeader/BlogHeader.vue'
+
 export default {
     name: 'app',
     data() {
         return {}
+    },
+    components: {
+        BlogHeaderComponent,
     },
 }
